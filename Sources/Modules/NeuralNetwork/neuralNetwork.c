eafd9f757b6ea9021c95f10c002a1033cff9a00d
Jack Lukomski <jtlukomski@gmail.com>
2023-05-07 21:25:21 -0400

    Merge from other branch

--- Sources/Modules/NeuralNetwork/neuralNetwork.c
+++ Sources/Modules/NeuralNetwork/neuralNetwork.c
@@ -37,47 +37,8 @@ NerualNetwork * xConstructNeuralNetwork (Matrix * inputMatrix, uint16_t numHidde
 
     for (uint16_t currHL = 0; currHL < numHiddenLayers; currHL++)
     {
-<<<<<<< HEAD
-        Layer * currHiddenLayer = (Layer *) malloc(sizeof(Layer));
-        currHiddenLayer->numNeurons = numHiddenNeurons[currLayer];
-        currHiddenLayer->neurons = (Neuron *) malloc(numHiddenNeurons[currLayer] * sizeof(Neuron));
-        for (uint16_t currNeuron = 0; currNeuron < numHiddenNeurons[currLayer]; currNeuron++)
-        {
-            currHiddenLayer->neurons[currNeuron].numInputs = numInputs;
-            currHiddenLayer->neurons[currNeuron].weights = (double *) malloc(numInputs * sizeof(double));
-            for (uint16_t currWeight = 0; currWeight < numInputs; currWeight++)
-            {
-                currHiddenLayer->neurons[currNeuron].weights[currWeight] = weights[currLayer][currNeuron][currWeight];
-            }
-            currHiddenLayer->neurons[currNeuron].bias = biases[currLayer][currNeuron];
-        }
-        hiddenLayers[currLayer] = *currHiddenLayer;
-        //free(currHiddenLayer);
-=======
         newNN->hiddenLayers[currHL] = xConstructHiddenLayer(hiddenLayerMatricies[currHL], hiddenLayerBiases[currHL]);
->>>>>>> RedoingNNModuleToIncoperateMatrix
     }
 
-<<<<<<< HEAD
-NeuralNetwork * xCreateNeuralNetwork(uint16_t numInputs, uint16_t numHiddenLayers, uint16_t * numHiddenNurons, uint16_t numOutputs, double *** hiddenWeights, double ** hiddenBiases, double ** outputWeights, double * outputBiases)
-{
-    Layer * inputLayer = xCreateInputLayer(numInputs);
-    Layer * hiddenLayers = xCreateHiddenLayers(numHiddenLayers, numHiddenNurons, numInputs, hiddenWeights, hiddenBiases);
-    Layer * outputLayer = xCreateOutputLayer(numOutputs, numHiddenNurons[numHiddenLayers-1], outputWeights, outputBiases);
-
-    NeuralNetwork * neuralNetwork = (NeuralNetwork *) malloc(sizeof(NeuralNetwork));
-    neuralNetwork->numInputs = numInputs;
-    neuralNetwork->numHiddenLayers = numHiddenLayers;
-    neuralNetwork->numOutputs = numOutputs;
-    neuralNetwork->inputLayer = *inputLayer;
-    neuralNetwork->hiddenLayers = hiddenLayers;
-    neuralNetwork->outputLayer = *outputLayer;
-
-    free(inputLayer);
-    free(outputLayer);
-
-    return neuralNetwork;
-=======
     return newNN;
->>>>>>> RedoingNNModuleToIncoperateMatrix
 }
